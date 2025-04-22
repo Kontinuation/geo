d7c2bda0baa29ad3cfe1be7f371426c36bb2cff5
Kristin Cowalcijk <bo@wherobots.com>
2025-04-22 11:16:00 +0800

    Added area bench for both geo and geo-generic-alg

--- geo-generic-alg/benches/area.rs
+++ geo-generic-alg/benches/area.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use geo::Area;
-use geo::Polygon;
+use geo_generic_alg::Area;
+use geo_generic_alg::Polygon;
 use geo_types::to_geo::ToGeoGeometry;
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -16,7 +16,15 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("area_wkb", |bencher| {
         let wkb_bytes = geo_test_fixtures::norway_main_wkb();
         bencher.iter(|| {
-            let wkb_geom = wkb::reader::read_wkb(&wkb_bytes).unwrap();
+            let wkb_geom = geo_generic_tests::wkb::reader::read_wkb(&wkb_bytes).unwrap();
+            criterion::black_box(wkb_geom.signed_area());
+        });
+    });
+
+    c.bench_function("area_wkb_convert", |bencher| {
+        let wkb_bytes = geo_test_fixtures::norway_main_wkb();
+        bencher.iter(|| {
+            let wkb_geom = geo_generic_tests::wkb::reader::read_wkb(&wkb_bytes).unwrap();
             let geom = wkb_geom.to_geometry();
             criterion::black_box(geom.signed_area());
         });
